e989117a7313b9fea1502dab1ac350d6195beb26
Fathu Rahman <51043165+fathurmdr@users.noreply.github.com>
2024-09-10 10:06:40 +0000

    create krakend api

--- admin/index.js
+++ admin/index.js
@@ -4,11 +4,41 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next)=>{
+  const start = Date.now();
+
+  res.on("finish",()=>{
+    const duration = Date.now() - start;
+    console.log(`Method: ${req.method} | Path: ${req.originalUrl} | Status: ${res.statusCode} | Duration: ${duration}`);
+  })
+
+  next()
+})
+
 app.get("/admin", (req, res) => {
   res.json({
     data: "Hello Admin World!",
   });
 });
+
+app.use((err, req, res, next)=>{
+  if (!err) {
+    next();
+    return;
+  }
+
+  console.error({
+    message: err.message,
+    detail: err.stack,
+    ip: req.ip,
+    method: req.method,
+    path: req.originalUrl,
+  });
+
+  res.status(400).json({
+    errorMsg: err.message
+  })
+})
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");

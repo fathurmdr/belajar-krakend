0b2e0e211eb1f40abac806c1485f4ed14f6ceefa
fathurmdr <fathur208@gmail.com>
2024-09-11 16:49:50 +0700

    fix pm2 config for krakend

--- krakend-web/ecosystem.config.js
+++ krakend-web/ecosystem.config.js
@@ -7,9 +7,9 @@ module.exports = {
       env: {
         KRAKEND_PORT: 4040,
         FC_ENABLE: 1,
-        FC_SETTINGS: "/manifests/configs",
-        FC_PARTIALS: "/manifests/partials",
-        FC_TEMPLATES: "/manifests/templates",
+        FC_SETTINGS: "/configs",
+        FC_PARTIALS: "/partials",
+        FC_TEMPLATES: "/templates",
       },
     },
   ],
